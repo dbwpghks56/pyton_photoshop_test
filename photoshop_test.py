1b7e148da08967a667ec906e96d43a74ab860f70
wpghks <wpghks56@naver.com>
2024-05-31 17:15:33 +0900

    [Feature] 요구사항에 맞게 변경

--- photoshop_test.py
+++ photoshop_test.py
@@ -1,40 +1,55 @@
 import os
 import re
 
+from PIL import Image
 from psd_tools import PSDImage
+from psd_tools.constants import BlendMode
 
+
+def save_layer_add_background(layer, path):
+    background = Image.new("RGBA", (1920, 1080), (255, 255, 255, 0))
+    image: Image
+    
+    if layer.is_group():
+        image = layer.composite()
+        
+    else:
+        image = layer.topil()
+
+    offset = layer.bbox[0:2]
+    background.paste(image, offset, image)
+    background.save(f"{path}/{layer.name}.png", format='PNG', optimize=True)
 
 def sanitize_directory_name(name):
     return re.sub(r'[<>:"/\\|?*]', '_', name)
+
 
 def save_layer_as_png(layer, path):
     # 레이어 이름을 안전하게 치환
     layer_name_sanitized = sanitize_directory_name(layer.name)
     layer_path = os.path.join(path, layer_name_sanitized)
-
+    # layer 변수 타입
+    # print(type(layer))
     # 레이어가 그룹인 경우 재귀적으로 하위 레이어 탐색
     if layer.is_group():
         if not os.path.exists(layer_path):
             os.makedirs(layer_path)
+            
+        layer.visible = True
         for child_layer in layer:
-            save_layer_as_png(child_layer, layer_path)
-    else:
-        try:
-            # 레이어를 PNG로 저장
-            if not os.path.exists(path):
-                os.makedirs(path)
-            image = layer.topil()
-            image_path = f"{layer_path}.png"
-            image.save(image_path)
-            print(f"Saved layer: {layer.name} as {image_path}")
-        except Exception as e:
-            print(f"Failed to save layer: {layer.name}")
-            print(e)
-            pass
+            
+            try:
+                save_layer_add_background(child_layer, layer_path)
+                    
+            except Exception as e:
+                print(f"Failed to save layer: {layer.name}")
+                print(e)
+                pass
+
 
 def main():
-    currPage = input("시작할 파일 이름 : ")
-    psd_file_path = 'C:\\Users\\wpghk\\Desktop\\pyton_photoshop_test\\' + currPage + '.psd'
+    # currPage = input("시작할 파일 이름 : ")
+    psd_file_path = f'C:\\Users\\wpghk\\Desktop\\pyton_photoshop_test\\[인하대]SDGs_02.psd'
     base_path = 'C:\\Users\\wpghk\\Desktop\\pyton_photoshop_test\\layers'
     
     psd = PSDImage.open(psd_file_path)
--- photoshop_test_false.py
+++ photoshop_test_false.py
@@ -1,41 +1,60 @@
-"""Export every layer as a .png file."""
-# Import built-in modules
 import os
+import re
 
-# Import third-party modules
-import photoshop.api as ps
-# Import local modules
-from photoshop import Session
+from PIL import Image
+from psd_tools import PSDImage
+from psd_tools.constants import BlendMode
 
 
-def hide_all_layers(layers):
-    for layer in layers:
-        layer.visible = False
+def save_layer_add_background(layer, path):
+    background = Image.new("RGBA", (1920, 1080), (255, 255, 255, 0))
+    image: Image
+    
+    if layer.is_group():
+        image = layer.composite()
+        
+    else:
+        image = layer.topil()
+
+    offset = layer.bbox[0:2]
+    background.paste(image, offset, image)
+    background.save(f"{path}/{layer.name}.png", format='PNG', optimize=True)
+
+def sanitize_directory_name(name):
+    return re.sub(r'[<>:"/\\|?*]', '_', name)
+
+
+def save_layer_as_png(layer, path):
+    # 레이어 이름을 안전하게 치환
+    layer_name_sanitized = sanitize_directory_name(layer.name)
+    layer_path = os.path.join(path, layer_name_sanitized)
+    # layer 변수 타입
+    # print(type(layer))
+    # 레이어가 그룹인 경우 재귀적으로 하위 레이어 탐색
+    if layer.is_group():
+        if not os.path.exists(layer_path):
+            os.makedirs(layer_path)
+            
+        layer.visible = True
+        for child_layer in layer:
+            
+            try:
+                save_layer_add_background(child_layer, layer_path)
+                    
+            except Exception as e:
+                print(f"Failed to save layer: {layer.name}")
+                print(e)
+                pass
 
 
 def main():
-    psd_file_path = 'C:\\Users\\wpghk\\Desktop\\pyton_photoshop_test\\images.psd'
-    base_path = 'C:\\Users\\wpghk\\Desktop\\pyton_photoshop_test'
-    with Session(psd_file_path, action="open") as ps:
-        doc = ps.active_document
-        options = ps.PNGSaveOptions()
-        options.compression = 1
-        layers = doc.artLayers
-        print("shoot")
-        for layer in layers:
-            print("what")
-            hide_all_layers(layers)
-            layer.visible = True
-            layer_path = os.path.join(base_path, layer.name)
-            print(layer_path)
-            if not os.path.exists(layer_path):
-                os.makedirs(layer_path)
-            image_path = os.path.join(layer_path, f"{layer.name}.png")
-            print(image_path)
-            doc.saveAs(image_path, options=options, asCopy=True)
-        ps.alert("Task done!")
-        ps.echo(doc.activeLayer)
-
+    # currPage = input("시작할 파일 이름 : ")
+    psd_file_path = f'C:\\Users\\wpghk\\Desktop\\pyton_photoshop_test\\[인하대]SDGs_02.psd'
+    base_path = 'C:\\Users\\wpghk\\Desktop\\pyton_photoshop_test\\layers'
+    
+    psd = PSDImage.open(psd_file_path)
+    for layer in psd:
+        save_layer_as_png(layer, base_path)
 
 if __name__ == "__main__":
     main()
